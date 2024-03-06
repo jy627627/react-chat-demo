d6342addd10607a4bbbf66401c547f08b26ff655
BRITNEY.姜二丫 <9908104+jy627627@user.noreply.gitee.com>
2024-03-06 17:36:31 +0800

    ..

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -2,12 +2,20 @@ import React from "react";
 import face from '../../images/face.jpg'
 import { AvatarClip, AvatarImage, StatusIcon, StyleAvatar } from './style'
 
-export default function Avatar() {
+export default function Avatar({
+   src,
+   size = '48px',
+   status,
+   statusIconSize = '8px',
+    ...rest
+}) {
     return (
-        <StyleAvatar>
-            <StatusIcon></StatusIcon>
-            <AvatarClip>
-                <AvatarImage src={face} alt="place"/>
+        <StyleAvatar {...rest}>
+            {status && (
+                <StatusIcon status={status} size={statusIconSize}></StatusIcon>
+            )}
+            <AvatarClip size={size}>
+                <AvatarImage src={src} alt="place"/>
             </AvatarClip>
         </StyleAvatar>
     )
